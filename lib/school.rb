ed73d98c263f6631e7bebc24b5511b6145b49e0b
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-30 00:47:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :name , :roster
+  
+    
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, gpa)
+    @roster[gpa] ||= []
+    @roster[gpa] << name 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    
+    sorted = {}
+    
+    @roster.each do |grade, students|
+      sorted[grade] = students.sort 
+    end 
+    sorted
+  end 
+  
+  
+end 
